c1861e45a96b7a222f1c7f988913256cf2b3a9ff
hareesh-KC <139694573+hareesh-KC@users.noreply.github.com>
2023-09-08 17:18:39 +0530

    Update aws_ec2.tf

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -3,11 +3,11 @@ region = "ap-south-1"
 }
 
 resource "aws_instance" "AWSServer"{
-ami = "ami-0da59f1af71ea4ad2"
+ami = "ami-02bb7d8191b50f4bb"
 instance_type = "t2.micro"
-key_name = "devopsmithuntechnologiesbhaskar"
+key_name = "redhatlinux"
 security_groups = ["launch-wizard-2"]
 tags = {
-Name = "tomcatservers"
+Name = "tomcatservers2"
 }
 }
